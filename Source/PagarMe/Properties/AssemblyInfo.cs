479c43612ac203a0c184784bf27338d83f197473
Jonathan Lima <greenboxal@gmail.com>
2013-11-22 00:33:49 -0200

    Basic skeleton.

---
+++ Source/PagarMe/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PagarMe")]
+[assembly: AssemblyDescription("Integration library for Pagar.me")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Retail")]
+#endif
+
+[assembly: AssemblyCompany("Pagar.me Pagamentos S.A.")]
+[assembly: AssemblyProduct("Pagar.me")]
+[assembly: AssemblyCopyright("Copyright © Pagar.me Pagamentos S.A. 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisibleAttribute(false)]
---
+++ Test/PagarMeTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PagarMe")]
+[assembly: AssemblyDescription("Integration services for Pagar.me [NUnit]")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Retail")]
+#endif
+
+[assembly: AssemblyCompany("Pagar.me Pagamentos S.A.")]
+[assembly: AssemblyProduct("Pagar.me")]
+[assembly: AssemblyCopyright("Copyright © Pagar.me 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisibleAttribute(false)]
