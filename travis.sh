cc9c4f1ba79004f23817edddfd6307f91c7beec1
Jonathan Lima <greenboxal@gmail.com>
2013-12-03 09:19:51 -0200

    Fix nunit invocation.

--- travis.sh
+++ travis.sh
@@ -7,4 +7,5 @@ export LD_LIBRARY_PATH="/opt/mono/bin:/opt/mono/lib:$LD_LIBRARY_PATH"
 
 mozroots --import --sync
 xbuild CI.proj
-mono --runtime=v4.0.30319 /usr/lib/nunit/nunit-console.exe ./tests/PagarMe.Tests/bin/Debug_Net40/PagarMe.Tests.dll -exclude NotWorkingOnMono
+mono --runtime=v4.0.30319 ./.nuget/NuGet.exe install -Version 2.6.3 -NonInteractive NUnit.Runners
+mono --runtime=v4.0.30319 ./NUnit.Runners.2.6.3/tools/nunit-console.exe ./tests/PagarMe.Tests/bin/Debug_Net40/PagarMe.Tests.dll -exclude NotWorkingOnMono
