2401fa672fc09ce2b9146b9c7c9618d0d01de400
Victor Gama <hey@vito.io>
2016-03-04 16:59:40 -0300

    Add Fingerprint check methods

---
+++ PagarMe.Tests/FingerprintTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using PagarMe;
+
+namespace PagarMe.Tests
+{
+	[TestFixture]
+	public class FingerprintTests : PagarMeTestFixture
+	{
+		[Test]
+		public void CheckFingerprint() {
+			var expectedResult = "fd29daad6c47ff78c1604395320b60bac87830cb";
+			var inputData = "{\"sample\":\"payload\",\"value\":true}";
+			Assert.AreEqual(PagarMe.Utils.CalculateRequestHash(inputData), expectedResult);
+			Assert.IsTrue(PagarMe.Utils.validateRequestSignature(inputData, expectedResult));
+		}
+	}
+}
