87db91cd36470aa23a32ca31b45dd7e2aed5e285
Alexandre de Souza <xadee.13@gmail.com>
2016-07-19 14:11:19 -0300

    Updated JSON attribute for CardCvv property from  to

--- PagarMe.Tests/SubscriptionTests.cs
+++ PagarMe.Tests/SubscriptionTests.cs
@@ -7,55 +7,79 @@ using NUnit.Framework;
 
 namespace PagarMe.Tests
 {
-    [TestFixture]
-    public class SubscriptionTests : PagarMeTestFixture
-    {
-        [Test]
-        public void CreateWithPlan()
-        {
-            var plan = CreateTestPlan();
+	[TestFixture]
+	public class SubscriptionTests : PagarMeTestFixture
+	{
+		[Test]
+		public void CreateWithPlan ()
+		{
+			var plan = CreateTestPlan ();
 
-            plan.Save();
+			plan.Save ();
 
-            Assert.AreNotEqual(plan.Id, 0);
+			Assert.AreNotEqual (plan.Id, 0);
 
-            var subscription = new Subscription
-            {
-                CardHash = GetCardHash(),
-                Customer = new Customer
-                {
-                    Email = "josedasilva@pagar.me"
-                },
-                Plan = plan
-            };
+			var subscription = new Subscription {
+				CardHash = GetCardHash (),
+				Customer = new Customer {
+					Email = "josedasilva@pagar.me"
+				},
+				Plan = plan
+			};
 
-            subscription.Save();
+			subscription.Save ();
 
-            Assert.AreEqual(subscription.Status, SubscriptionStatus.Paid);
-        }
+			Assert.AreEqual (subscription.Status, SubscriptionStatus.Paid);
+		}
 
-        [Test]
-        public void CancelSubscription()
-        {
-            var plan = CreateTestPlan();
-            plan.Save();
+		[Test]
+		public void CreateWithPlanAndNewCard ()
+		{
+			var plan = CreateTestPlan ();
 
-            Assert.AreNotEqual(plan.Id, 0);
+			plan.Save ();
 
-            var subscription = new Subscription
-            {
-                CardHash = GetCardHash(),
-                Customer = new Customer
-                {
-                    Email = "josedasilva@pagar.me"
-                },
-                Plan = plan
-            };
+			Assert.AreNotEqual (plan.Id, 0);
 
-            subscription.Save();
-            subscription.Cancel();
+			var subscription = new Subscription {
+				CardCvv = "651",
+				CardExpirationDate = "0921",
+				CardHolderName = "Jose da Silva",
+				CardNumber = "4242424242424242",
+				Customer = new Customer {
+					Email = "josedasilva@pagar.me"
+				},
+				Plan = plan
+			};
 
-            Assert.AreEqual(subscription.Status, SubscriptionStatus.Canceled);
-        }
-    }
+			try {
+				subscription.Save ();
+				Assert.Fail ();
+			} catch (PagarMeException ex) {
+				Assert.AreEqual (ex.Error.Errors [0].Message, "Não foi possível realizar uma transação nesse cartão de crédito.");	
+			}
+		}
+
+		[Test]
+		public void CancelSubscription ()
+		{
+			var plan = CreateTestPlan ();
+			plan.Save ();
+
+			Assert.AreNotEqual (plan.Id, 0);
+
+			var subscription = new Subscription {
+				CardHash = GetCardHash (),
+				Customer = new Customer {
+					Email = "josedasilva@pagar.me"
+				},
+				Plan = plan
+			};
+
+			subscription.Save ();
+			subscription.Cancel ();
+
+			Assert.AreEqual (subscription.Status, SubscriptionStatus.Canceled);
+		}
+	}
 }
