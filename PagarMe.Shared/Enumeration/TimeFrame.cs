fda3df162268d78575a7d55d8c736f43fa238de3
Fellipe <fellipe.thufik@pagar.me>
2016-12-14 18:08:37 -0200

    inicio da implementação dos bulk anticipations

---
+++ PagarMe.Shared/Enumeration/TimeFrame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PagarMe.Enumeration
+{
+    public enum TimeFrame
+    {
+        [Base.EnumValue("start")] Start,
+        [Base.EnumValue("end")] End
+
+    }
+}
---
+++ PagarMe.Shared/Model/BulkAnticipation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PagarMe.Enumeration;
+
+namespace PagarMe.Model
+{
+    public class BulkAnticipation : Base.Model
+    {
+        protected override string Endpoint { get { return "/bulk_anticipations"; } }
+
+        public BulkAnticipation() : this(null) { }
+
+        public BulkAnticipation(PagarMeService service) : base(service) { }
+
+        public BulkAnticipationStatus Status
+        {
+            get { return GetAttribute<BulkAnticipationStatus>("status"); }
+            set { SetAttribute("status", value); }
+        }
+
+        public TimeFrame Timeframe
+        {
+            get { return GetAttribute<TimeFrame>("timeframe"); }
+            set { SetAttribute("timeframe", value); }
+        }
+
+        public string PaymentDate
+        {
+            get { return GetAttribute<string>("payment_date"); }
+            set { SetAttribute("payment_date", value); }
+        }
+
+        public int Amount
+        {
+            get { return GetAttribute<int>("amount"); }
+            set { SetAttribute("amount", value); }
+        }
+
+        public int Fee
+        {
+            get { return GetAttribute<int>("fee"); }
+            set { SetAttribute("fee", value); }
+        }
+
+        public int AnticipationFee
+        {
+            get { return GetAttribute<int>("anticipation_fee"); }
+            set { SetAttribute("anticipation_fee", value); }
+        }
+    }
+}
